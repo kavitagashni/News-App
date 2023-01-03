d855ba3b0f9e96f549cb6bb8c936dc168ac2c902
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2023-01-03 16:18:02 +0530

    Update NewsComp.js

--- src/component/NewsComp.js
+++ src/component/NewsComp.js
@@ -5,7 +5,7 @@ import React from 'react'
 function NewsComp(props) {
     return (
         <div className="all-cards">
-            {props.userData.length > 0 ? props.userData.map((value, index) => {
+            {props.userData && props.userData.length && props.userData.map((value, index) => {
                 return (
                     <a href={value.url} target="_blank" key={index}>
                         <div className='newscard'>
@@ -25,7 +25,7 @@ function NewsComp(props) {
                         </div>
                     </a>
                 )
-            }) : ''}
+            })}
         </div>
     )
 }
