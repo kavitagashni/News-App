9f564070ad6c3f8a7c4aa5b5ce14105a8d194477
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2023-01-03 15:58:38 +0530

    Add files via upload
    correct map function

---
+++ src/component/NewsComp.js
@@ -0,0 +1,32 @@
+import './NewsComp.css';
+import React from 'react'
+
+
+function NewsComp(props) {
+    return (
+        <div className="all-cards">
+            {props.userData.length > 0 ? props.userData.map((value, index) => {
+                return (
+                    <a href={value.url} target="_blank" key={index}>
+                        <div className='newscard'>
+                            <img src={value.urlToImage} className='news-img' />
+                            <div>
+                                <p><b>Content: </b>{value.content}</p>
+                                <p><b>description: </b> {value.description}</p>
+                            </div>
+                            <div className='news-info'>
+                                <div><b>publishedAt:  </b>{value.publishedAt}</div>
+                                <div>
+                                    <p><b>author:  </b>{value.author}</p>
+                                    <p><b>source:  </b>{value.source.name} </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </a>
+                )
+            }) : ''}
+        </div>
+    )
+}
+export default NewsComp;
